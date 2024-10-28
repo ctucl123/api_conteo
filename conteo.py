d3fecede277967721d49c794eb53466bdfa386c8
David Diaz <ctucl-desarrollo@outlook.com>
2024-10-28 08:40:05 -0500

    funciona

--- conteo.py
+++ conteo.py
@@ -6,15 +6,15 @@ cap = cv2.VideoCapture(video_path)
 if not cap.isOpened():
     print("Error al abrir el video.")
     exit()
-
-
 model_path = "models/best.onnx"
 yolov8_detector = YOLOv8(model_path, conf_thres=0.5, iou_thres=0.5)
-
+cv2.namedWindow("Detected Objects", cv2.WINDOW_NORMAL)
 while cap.isOpened():
     if cv2.waitKey(1) == ord('q'):
         break
+
     try:
+   
         ret, frame = cap.read()
         if not ret:
             break
@@ -22,4 +22,6 @@ while cap.isOpened():
         print(e)
         continue
     boxes, scores, class_ids = yolov8_detector(frame)
-    print(scores)
+    combined_img = yolov8_detector.draw_detections(frame)
+    cv2.imshow("Detected Objects", combined_img)
+
--- test/cnn_optimized_v3.py
+++ test/cnn_optimized_v3.py
@@ -6,15 +6,10 @@ cap = cv2.VideoCapture(video_path)
 if not cap.isOpened():
     print("Error al abrir el video.")
     exit()
-
-
 model_path = "models/best.onnx"
 yolov8_detector = YOLOv8(model_path, conf_thres=0.5, iou_thres=0.5)
-
 cv2.namedWindow("Detected Objects", cv2.WINDOW_NORMAL)
 while cap.isOpened():
-
-
     if cv2.waitKey(1) == ord('q'):
         break
 
